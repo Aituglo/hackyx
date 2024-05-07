8895334a7b6c43da7d4fc504335ab8aa3379ad34
Wlayzz <34021743+wlayzz@users.noreply.github.com>
2024-05-07 16:42:42 +0200

    Finish parsing for huntr

--- scraper/huntr.py
+++ scraper/huntr.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from utils.yamler import write_yaml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -10,7 +11,6 @@ import time
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from utils.souper import souper
-import json
 
 def get_last_date(soup):
     rows = soup.find_all("tr")
@@ -25,16 +25,17 @@ def get_last_date(soup):
     parsed_dates = [parse_date(date) for date in dates]
     min_date = min(parsed_dates)
     print("Last hacktivity retrieve:", min_date.strftime('%B %d, %Y'))
+    return min_date
 
-def parse_html(html, reports):
+def parse_html(html):
+    reports = []
     soup = BeautifulSoup(html, 'html.parser')
     reports_link = soup.find_all('a', id='report-link')
     
     for r in reports_link:
-        reports.append({"link":r['href'], "name":r.get_text() })
+        reports.append({"link":r['href'], "title":r.get_text() })
 
-    with open('data.json', 'w') as f:
-        json.dump(reports, f)
+    return reports
 
 def parse_date(date_str):
     # Replace suffixes to create a date format that can be parsed
@@ -44,39 +45,98 @@ def parse_date(date_str):
                         .replace('th', ''))
     return parser.parse(date_str)
 
-def capture_web_content(url):
-    reports = []
-
-    parsed_url = urlparse(url)
-    fragment = parsed_url.fragment
-    
+def get_chrome_driver():
     options = Options()
     options.add_argument("--headless")
 
     webdriver_service = Service(ChromeDriverManager().install())
 
-    driver = webdriver.Chrome(service=webdriver_service, options=options)
+    return webdriver.Chrome(service=webdriver_service, options=options)
+
+def parse_hacktivity():
+    url = f"https://huntr.com/bounties/hacktivity"
+
+    driver = get_chrome_driver()
     try:
         driver.get(url)
         
         time.sleep(3)
 
+        last_date = ''
+        
         while driver.find_element(By.ID,'show-more-button'):
             driver.find_element(By.ID,'show-more-button').click()
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
-            get_last_date(soup)
+            date = get_last_date(soup)
+
+            if date == last_date:
+                break
+            else:
+                last_date = date
             time.sleep(3)
-        
-        parse_html(html, reports)
-        
         driver.quit()
+        return parse_html(html)
+        
+    except Exception as e:
+        print("An error has occured:  ", e)
+        driver.quit()
+        return None
+
+def parse_report(url, title):
+    driver = get_chrome_driver()
+    try:
+        driver.get(url)
+        time.sleep(3)
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # program
+        program = soup.find(id='title').find('a').text
+        infos = soup.find(id='actions-and-stats')
+        links = infos.find_all('a')
+        cve = None
+        cwe = None
+        for link in links:
+            if 'CVE' in link.text:
+                cve = link.text
+            if 'CWE' in link.text:
+                cwe = link.text
+        body = soup.find('div', class_='markdown-body')
+        content = body.text
+        description = None
+        for element in body.descendants:
+            if element.name and 'Description' in element.get_text(strip=True):
+                # Find the next sibling element that is a tag
+                next_tag = element.find_next_sibling()
+                if next_tag:
+                    description = next_tag.text
+                    break
+        vuln = cwe.split(':', 1)[1].strip()
+
+        driver.quit()
+        return {
+            'title': title,
+            'description': description,
+            'url': url,
+            'program': program,
+            'content': content,
+            'cve': cve,
+            'source': 'Huntr', 
+            'cwe': cwe,
+            'tags': ['Open Source', vuln]
+        }
+
     except Exception as e:
         print("An error has occured:  ", e)
         driver.quit()
         return None
 
 if __name__ == "__main__":
-    url = f"https://huntr.com/bounties/hacktivity"
-
-    capture_web_content(url)
+    # tag opensource, source: huntr
+    hacktivity = parse_hacktivity()
+    for hack in hacktivity:
+        url = 'https://huntr.com' + hack['link']
+        report = parse_report(url, hack['title'])
+        write_yaml(report)
---
+++ scraper/utils/yamler.py
@@ -0,0 +1,42 @@
+import yaml
+import os
+
+def write_yaml(new_content):
+    # Define the new content to be added example
+    """
+    new_content = {
+        'title': 'Titre 2',
+        'description': 'Description 2',
+        'url': 'http://url2.com',
+        'program': 'xxx',
+        'cve': 'CVE-XXXX-XXXXX',
+        'source': 'platforme', 
+        'cwe': 'CWE-XX: xxx',
+        'tags': ['tag3', 'tag4']
+    }
+    """ 
+
+    # File to read and write
+    filename = 'content/to_add.yml'
+
+   # Check if the file exists
+    if os.path.exists(filename):
+        # File exists, read the existing data
+        with open(filename, 'r') as file:
+            data = yaml.safe_load(file) or {}  # Use safe_load to load the YAML, ensure it's not None
+    else:
+        # File does not exist, initialize an empty dictionary
+        data = {}
+    
+    # Check if the 'contents' key exists, and if not, create it
+    if 'contents' not in data:
+        data['contents'] = []
+
+    # Append the new content
+    data['contents'].append(new_content)
+
+    # Write the updated data back to the YAML file
+    with open(filename, 'w') as file:
+        yaml.safe_dump(data, file, allow_unicode=True)
+
+    print(f'Updated data has been written to {filename}')
